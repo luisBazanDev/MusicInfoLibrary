a52fd202fe6deb33922233f5d4f085be391547cf
Luis Bazan <luis@bazan.pe>
2023-03-23 20:52:21 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Music Info Library
+
+# Contributors
+- Luis Bazan Rios
---
+++ src/main/java/pe/edu/utp/Main.java
@@ -0,0 +1,7 @@
+package pe.edu.utp;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
